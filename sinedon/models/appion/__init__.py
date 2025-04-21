983fee2593db3a4dd19be0bffb7e852f97bb9b4f
John Pellman <john+github@libjpel.so>
2025-04-21 11:52:17 -0400

    Add various init files

---
+++ sinedon/models/appion/__init__.py
@@ -0,0 +1,22 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright 2025 New York Structural Biology Center
+
+from .ApAceRunData import ApAceRunData
+from .ApAppionJobData import ApAppionJobData
+from .ApAssessmentRunData import ApAssessmentRunData
+from .ApCtfData import ApCtfData
+from .ApCtfFind4ParamsData import ApCtfFind4ParamsData
+from .ApCtfTiltParamsData import ApCtfTiltParamsData
+from .ApDDAlignImagePairData import ApDDAlignImagePairData
+from .ApDDAlignStatsData import ApDDAlignStatsData
+from .ApDDFrameTrajectoryData import ApDDFrameTrajectoryData
+from .ApDDStackParamsData import ApDDStackParamsData
+from .ApDDStackRunData import ApDDStackRunData
+from .ApPathData import ApPathData
+from .ApSymmetryData import ApSymmetryData
+from .ScriptHostName import ScriptHostName
+from .ScriptParamName import ScriptParamName
+from .ScriptParamValue import ScriptParamValue
+from .ScriptProgramName import ScriptProgramName
+from .ScriptProgramRun import ScriptProgramRun
+from .ScriptUserName import ScriptUserName
