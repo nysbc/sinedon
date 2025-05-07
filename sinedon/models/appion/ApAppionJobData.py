a55626ca63ec76cc5c5b134799942dd8dc414bfa
John Pellman <john+github@libjpel.so>
2025-05-07 09:54:45 -0400

    Update ApAppionJobData

--- sinedon/models/appion/ApAppionJobData.py
+++ sinedon/models/appion/ApAppionJobData.py
@@ -2,41 +2,31 @@
 # Copyright 2025 New York Structural Biology Center
 
 from django.db import models
-from .ApPathData import ApPathData
-from ..leginon import SessionData
 
 class ApAppionJobData(models.Model):
 	def_id = models.AutoField(db_column="DEF_id", primary_key=True)
 	def_timestamp = models.DateTimeField(
 		db_column="DEF_timestamp", auto_now_add=True
 	)
-	ref_appathdata_path = models.ForeignKey(
-		ApPathData,
+	ref_appathdata_path = models.IntegerField(
 		db_column="REF|ApPathData|path",
 		blank=True,
 		null=True,
-		on_delete=models.DO_NOTHING,
 	)
-	ref_sessiondata_session = models.ForeignKey(
-		SessionData,
+	ref_sessiondata_session = models.IntegerField(
 		db_column="REF|SessionData|session",
 		blank=True,
 		null=True,
-		on_delete=models.DO_NOTHING,
 	)
-	ref_appathdata_dmfpath = models.ForeignKey(
-		ApPathData,
+	ref_appathdata_dmfpath = models.IntegerField(
 		db_column="REF|ApPathData|dmfpath",
 		blank=True,
 		null=True,
-		on_delete=models.DO_NOTHING,
 	)
-	ref_appathdata_clusterpath= models.ForeignKey(
-		ApPathData,
+	ref_appathdata_clusterpath= models.IntegerField(
 		db_column="REF|ApPathData|clusterpath",
 		blank=True,
 		null=True,
-		on_delete=models.DO_NOTHING,
 	)
 	name = models.TextField(blank=True,null=True)
 	cluster = models.TextField(blank=True,null=True)
