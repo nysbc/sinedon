6857498485df35f9833b8bf40bf453d630c9c6cc
John Pellman <john+github@libjpel.so>
2025-07-22 11:41:15 -0400

    get should return the most recent record if multiple records exist

--- sinedon/base/get.py
+++ sinedon/base/get.py
@@ -1,4 +1,5 @@
 from .private import _getModel, _getSerializer
+from django.db.models import F
 
 def get(model_name : str, data : dict = {}):
     model=_getModel(model_name)
@@ -9,6 +10,8 @@ def get(model_name : str, data : dict = {}):
         raise RuntimeError("No serializer exists for model named %s." % model_name)
     try:
         record=model.objects.get(**data)
+    except model.MultipleObjectsReturned:
+        record=model.objects.filter(**data).order_by(F("def_timestamp").desc())[-1]
     except model.DoesNotExist:
         return {}
     ser=serializer(record)
